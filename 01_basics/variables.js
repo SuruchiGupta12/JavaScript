4dae1d66ec22c70f53c2634d3149a1197c09bdea
SuruchiGupta12 <121124481+SuruchiGupta12@users.noreply.github.com>
2024-08-28 19:20:39 +0000

    container config done

---
+++ 01_basics/variables.js
@@ -0,0 +1,12 @@
+const accountId = 124545
+let accountEmail = "suru@google.com"
+var accountCity = "Jaipur"
+accountState = "Wb"     //not used
+
+//using var is not recommended
+console.log(accountEmail)
+
+
+accountState = "india"
+
+console.table([accountId,accountState])
