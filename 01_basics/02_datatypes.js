01d6c308a794e7233ea516f3a72b6cb385ccc1fa
SuruchiGupta12 <121124481+SuruchiGupta12@users.noreply.github.com>
2024-08-30 07:06:16 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,25 @@
+"use strict";   //used in JS of newer version
+
+// alert( 3 + 3)   //used in browser not in nodejs
+
+let name = "Suru"   //string datatype
+let age = 18        //number
+let loggedIn = true  //boolean - true/false
+let state;
+
+//number => 2 to power 53
+// string => "" /''
+//boolean =>true/false
+// null => standalone value, represents an object as op
+//symbol => for uniqueness
+// undefined 
+//object
+
+//NOTE: null is an object
+
+console.log(typeof 's');     //string
+console.log(typeof age);    //number
+console.log(typeof null);   //object
+console.log(typeof undefined);  //undefined
+
+// diff bw null and undefined is in undefined val is not assigned and in null val is not present it is empty
