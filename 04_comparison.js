5d0a238a7b55be45a8e1293e5ec87bc7a77e01cd
SuruchiGupta12 <121124481+SuruchiGupta12@users.noreply.github.com>
2024-08-30 20:08:44 +0000

    comaprison

---
+++ 04_comparison.js
@@ -0,0 +1,24 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log(null > 0);  //false 0>0
+//null is treated as 0 in comaprison operator , else null
+console.log(null == 0); //false 
+console.log(null >= 0); //true as 0>=0
+
+
+//undfined is same for all it gives false
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// === checks both value and datatype
+
+console.log("2" === 2);
